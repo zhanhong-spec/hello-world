5c4f094b79fc789c45e2b5976e0202101f2b40fd
zhanhong-spec <limhong2001.gmail.com>
2020-04-20 17:08:19 +0800

    Modified Mobile.java file is in First Branch

--- Mobile.java
+++ Mobile.java
@@ -1,6 +1,7 @@
 class Mobile{
 
     public static void main(String[] args){
-        System.out.println("Hello World");
+        System.out.println("It is a mobile application");
+        System.out.println("This is for communication among users");
     }
 }
